8275e4d772b33813e75d6a6f26c687a74daa1364
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-08-01 22:28:31 +0200

    rearranged the points in their respective clusters

--- driver.cpp
+++ driver.cpp
@@ -4,9 +4,12 @@
 #include <fstream>
 #include <vector>
 #include <math.h>
+#include <map>
 #include "Point.h"
+#include "Cluster.h"
 
 using namespace std;
+using namespace RHMMUH005;
 
 struct Points {
 	float x, y;
@@ -26,8 +29,18 @@ int main(int argc, char* argv[]) {
 	char comma;
 
 	//vector<Points> point;
-	vector<RHMMUH005::Point> point;
-	RHMMUH005::Point coords;
+	vector<Point> point;
+	Point coords;
+
+	vector<Cluster> clusters;
+
+	vector<float> cluster1Distances;
+	vector<float> cluster2Distances;
+	vector<float> cluster3Distances;
+
+	vector<Point> group_one;
+	vector<Point> group_two;
+	vector<Point> group_three;
 
 	ifstream file("input");
 
@@ -35,11 +48,51 @@ int main(int argc, char* argv[]) {
 		point.push_back(coords);
 	}
 
-	for (int i = 0; i < point.size(); i++) {
-		cout << point.at(i).x << " " << point.at(i).y << endl;
+	clusters.push_back(Cluster(point.at(0), 1));
+	clusters.push_back(Cluster(point.at(3), 2));
+	clusters.push_back(Cluster(point.at(6), 3));
+
+	for (int i = 0; i < clusters.size(); i++) {
+		for (int j = 0; j < point.size(); j++) {
+			if (i == 0) {
+				cluster1Distances.push_back(distance(point.at(j), clusters.at(i).getPoints()));
+			}
+
+			if (i == 1) {
+				cluster2Distances.push_back(distance(point.at(j), clusters.at(i).getPoints()));
+			}
+
+			if (i == 2) {
+				cluster3Distances.push_back(distance(point.at(j), clusters.at(i).getPoints()));
+			}
+		}
 	}
 
-	cout << distance(point[0], point[4]) << endl;
+	for (int i = 0; i < cluster1Distances.size(); i++) {
+		if (cluster1Distances.at(i) < cluster2Distances.at(i)) {
+			if (cluster1Distances.at(i) < cluster3Distances.at(i)) {
+				group_one.push_back(point.at(i));
+			}
+
+			else {
+				group_three.push_back(point.at(i));
+			}
+		}
+
+		else if (cluster2Distances.at(i) < cluster3Distances.at(i)) {
+			group_two.push_back(point.at(i));
+		}
+
+		else {
+			group_three.push_back(point.at(i));
+		}
+	}
+
+	/*
+	for (int i = 0; i < group_three.size(); i++) {
+		cout << group_three.at(i).x << " " << group_three.at(i).y << endl;
+	}
+	*/
 
 	return 0;
 }
